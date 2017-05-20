6c02734ff4cd766d8379acaa03a5d14f5df2c34c
Michael Toth <michaeltoth@Michaels-iMac.local>
2017-05-19 22:36:42 -0400

    db

---
+++ db/schema.rb
@@ -0,0 +1,113 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20120215052548) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "concerts", force: :cascade do |t|
+    t.string "title"
+    t.datetime "date"
+    t.decimal "price"
+    t.float "lat"
+    t.float "lng"
+    t.integer "venue_id"
+    t.integer "group_id"
+    t.string "webpage"
+    t.string "zip"
+    t.float "latitude"
+    t.float "longitude"
+    t.boolean "offer"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "title"
+    t.string "contact"
+    t.string "phone"
+    t.string "street1"
+    t.string "street2"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "picture_file_name"
+    t.string "picture_content_type"
+    t.datetime "picture_updated_at"
+    t.integer "picture_file_size"
+    t.string "website"
+    t.string "email"
+    t.boolean "locked"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups_users", id: false, force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login", null: false
+    t.string "email", null: false
+    t.string "crypted_password", null: false
+    t.string "password_salt", null: false
+    t.string "persistence_token", null: false
+    t.string "single_access_token", null: false
+    t.string "perishable_token", null: false
+    t.integer "login_count", default: 0, null: false
+    t.integer "failed_login_count", default: 0, null: false
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string "current_login_ip"
+    t.string "last_login_ip"
+    t.boolean "notify"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "zip"
+    t.float "latitude"
+    t.float "longitude"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["login"], name: "index_users_on_login", unique: true
+    t.index ["persistence_token"], name: "index_users_on_persistence_token", unique: true
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.string "contact"
+    t.string "phone"
+    t.string "email"
+    t.string "street1"
+    t.string "street2"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.decimal "budget"
+    t.string "vpic_file_name"
+    t.string "vpic_content_type"
+    t.integer "vpic_file_size"
+    t.datetime "vpic_updated_at"
+    t.integer "user_id"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "website"
+  end
+
+end
